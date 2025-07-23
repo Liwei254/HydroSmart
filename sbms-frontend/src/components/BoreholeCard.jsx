288c7c7aa14ee9e5b0fbdc3b1d4703656b96e6d6
James Otipa <otipajames2@gmail.com>
2025-07-24 02:03:53 +0300

    Delete sbms-frontend directory

--- sbms-frontend/src/components/BoreholeCard.jsx
+++
@@ -1,9 +0,0 @@
-const BoreholeCard = ({ borehole }) => (
-  <div className="border rounded-xl shadow p-4 bg-white">
-    <h2 className="text-lg font-semibold">{borehole.name}</h2>
-    <p>Water Level: {borehole.waterLevel}m</p>
-    <p>Status: {borehole.status}</p>
-    <p>Last Update: {borehole.last_updated}</p>
-  </div>
-);
-export default BoreholeCard;
